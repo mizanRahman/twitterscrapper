4a7b84b59bf73b08f425f3352fbe1c841ea09514
Mizan Rahman <mizan.cse.buet@gmail.com>
2011-08-20 03:42:22 +0600

    initial

---
+++ twitterscrapp.py
@@ -0,0 +1,48 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      mizan
+#
+# Created:     19/08/2011
+# Copyright:   (c) mizan 2011
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+from BeautifulSoup import BeautifulSoup
+import urllib
+#code only looks at one page of followers instead of continuing to all of a user's followers
+#decided to only use a small sample
+
+site = "http://mobile.twitter.com"
+friends = set()
+response = urllib.urlopen(site)
+html = response.read()
+soup = BeautifulSoup(html)
+
+
+mainTrend = soup.find('div',{'class' : 'main-trend'})
+
+links= mainTrend.findAll('a',{'href':True})
+
+for link in links:
+    linkLabel=link.renderContents().lower()
+    url=str(link['href'])
+    print "--------------------", url,"----------------------------------------------------"
+
+    html = urllib.urlopen(url).read()
+    soup = BeautifulSoup(html)
+    tweets = soup.findAll('span',{'class' : 'status'})
+    for tweet in tweets:
+        for content in tweet.contents:
+            print str(content)
+
+
+
+
+
+
+
+print ("have a good coding")
+
